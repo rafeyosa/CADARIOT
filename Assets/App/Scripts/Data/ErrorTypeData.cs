7bf3d4985efce899866612b565c6b9cfeeae1add
Rahmat Febri Yoga Saputra <67069286+rafeyosa@users.noreply.github.com>
2023-05-30 21:40:25 +0700

    feat: update error description

--- Assets/App/Scripts/Data/ErrorTypeData.cs
+++ Assets/App/Scripts/Data/ErrorTypeData.cs
@@ -7,19 +7,19 @@ public static class ErrorTypeData {
         new ErrorModel {
             errorCode = "eN", 
             message = "Error: New Detect!",
-            description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus elementum ante tortor, a ultricies tellus pulvinar et. Cras hendrerit justo gravida velit tincidunt, id ullamcorper sapien eleifend. Maecenas ullamcorper interdum elit, eget cursus justo auctor non. Aenean imperdiet lectus et elit porta sollicitudin a eget orci.",
+            description = "New Item Detected, queue got messed up, maybe you need to check it.",
             createdAt = ""
         },
         new ErrorModel {
             errorCode = "eL", 
             message = "Error: Lost Item Color",
-            description = "Vivamus ex felis, eleifend et arcu in, rhoncus commodo enim. Cras sodales tortor eu ultrices laoreet. In vel venenatis quam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam luctus interdum felis, id accumsan odio pellentesque et. Quisque a velit leo.",
+            description = "Missing items, the machine will reset again in a few seconds, you need to check what happened to the machine.",
             createdAt = ""
         },
         new ErrorModel {
             errorCode = "eR", 
             message = "Error: Failed Read Color",
-            description = "Pellentesque commodo, dolor gravida dignissim consectetur, turpis augue iaculis purus, vitae eleifend nisi lectus at urna. In bibendum mauris at neque accumsan, at dapibus erat auctor. Curabitur lobortis nisi sit amet est consectetur, non eleifend urna finibus.",
+            description = "Failed to read item color. Item color not identified, will be forwarded.",
             createdAt = ""
         }
     };
